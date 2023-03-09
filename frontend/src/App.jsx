77e5de502bc8301fc217bcc51c3babd42f974863
sasha v <volfym79@gmail.com>
2023-03-09 23:53:33 +0100

    create basic form, without inputs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,33 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { useState } from 'react'
+
+const Title = () => {
+  const [title, setTitle] = useState('Title')
+  const [isTitleFocused, setIsTitleFocused] = useState(false)
+
+  return (
+    <div>
+      {!isTitleFocused
+        ? (
+          <h2 onClick={() => {setIsTitleFocused(true)}}
+            className='text-4xl flex-shrink'>
+            {title}
+          </h2>
+        )
+        : (
+          <input type="text" 
+            autoFocus
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+            onBlur={() => setIsTitleFocused(false)}
+            className='text-4xl flex-shrink'
+          />
+        )
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -7,8 +35,24 @@ function App() {
       <Header />
       <hr className="my-4 bg-black"/>
 
-      <main className="flex-grow w-full">
-        This is main
+      <main className="flex-grow w-full my-4 flex flex-col items-center">
+        <form action="" className='h-full flex flex-col justify-between'>
+          <Title />
+          <div>
+            <h2>INGREDIENTS</h2>
+          </div>
+
+          <div>
+            <h2>PROCESS</h2>
+          </div>
+
+          <div>
+            <h2>Tasting Notes / Description</h2>
+          </div>
+
+
+          <button type="submit" onClick={event => event.preventDefault()}>Add Recipe</button>
+        </form>
       </main>
 
       <hr className="my-4"/>
