d58816438b1675be628dca8a1bcfe9c05ed0a729
sasha v <volfym79@gmail.com>
2023-03-10 18:39:18 +0100

    add attribution

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Amaro Builder Site
 A Liqueur and Syrup recipe builder site using React (with Vite) and TailwindCSS.
+
+Original concept is a spreadsheet by reverblueflame available [here](https://docs.google.com/spreadsheets/d/1AhiRiem8g8Quiu_OruYpW2sxqpdqvNajwGk9833c734/edit#gid=287178337).
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => (
         GitHub
       </a>.
     </div>
+    <div>Idea based off a spreadsheet by reverblueflame available <a className="text-blue-400" href="https://docs.google.com/spreadsheets/d/1AhiRiem8g8Quiu_OruYpW2sxqpdqvNajwGk9833c734/edit#gid=287178337">here</a>.</div>
     <div>Copyleft <span className="rotate-180 inline-block text-sm">&copy;</span> 2023. Free to use and distribute with attribution.</div>
   </footer>
 )
