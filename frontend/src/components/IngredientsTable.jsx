36bb19137379b9e7d814738dea0a0f8e4b5b4a03
sasha v <volfym79@gmail.com>
2023-03-11 13:43:18 +0100

    impl autocomplete with react-select

--- frontend/src/components/IngredientsTable.jsx
+++ frontend/src/components/IngredientsTable.jsx
@@ -1,9 +1,12 @@
 import { DndContext, closestCenter } from '@dnd-kit/core'
 import { useSortable, arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { CSS } from '@dnd-kit/utilities'
-
 import { PropTypes } from 'prop-types'
+import TextInput from './TextInput'
+import CreatableSelect from 'react-select/Creatable'
+
+const generateId = () => `${Math.floor((Math.random()*1000))}`
 
 
 {/* data structure
@@ -17,6 +20,30 @@ th>
 <th >Amount (g)</th>
 <th /> 
 */}
+
+const fakeIng = [
+  {
+    name: 'Wormwood',
+    description: 'Bittering agent',
+    amount: '1',
+    isDry: true,
+    id: '1',
+  },
+  {
+    name: 'Vodka',
+    description: 'solvent',
+    amount: '300',
+    isDry: false,
+    id: '2',
+  },
+  {
+    name: 'Orange peel',
+    description: 'citrus fruit',
+    amount: '6',
+    isDry: true,
+    id: '3',
+  },
+]
 
 const DraggableRow = (props) => {
   const {
@@ -41,39 +68,74 @@ const DraggableRow = (props) => {
       <td {...attributes} {...listeners}><img src='drag.svg' className='h-4 inline'/></td>
       <td>{name}</td>
       <td>{description}</td>
-      <td>{amount}{isDry ? 'g' : 'ml'}</td>
-      <td><button onClick={() => console.log('remove', id)}>&#128465;</button></td>
+      <td>
+        <TextInput
+          state={amount}
+          setter={(newAmt) => props.changeAmount(id, newAmt)}
+          classes='w-12'
+        />
+        {isDry ? 'g' : 'ml'}
+      </td>
+      <td><button onClick={() => props.handleRemoveIngredient(id)}>&#128465;</button></td>
     </tr>
   )
 }
 DraggableRow.propTypes = {
-  ingredient: PropTypes.object.isRequired
+  ingredient: PropTypes.object.isRequired,
+  handleRemoveIngredient: PropTypes.func.isRequired,
+  changeAmount: PropTypes.func.isRequired,
 }
 
+const createOption = (label, id) => ({
+  label,
+  id,
+  value: label.toLowerCase().replace(/\W/g, ''),
+})
+
+let defaultOptions = []
+fakeIng.forEach(i => {
+  const opt = createOption(i.name, i.id)
+  defaultOptions.push(opt)
+})
+
+const CreatableIngredient = () => {  
+  const [options, setOptions] = useState(defaultOptions)
+  const [value, setValue] = useState({})
+
+  const handleCreate = (inputValue) => {
+    setTimeout(() => {
+      const newOption = createOption(inputValue, generateId())
+      setOptions(options.concat(newOption))
+      setValue(newOption)
+    }, 1000)
+
+  }
+
+  return (
+    <CreatableSelect
+      isClearable
+      onChange={newValue => setValue(newValue)}
+      onCreateOption={handleCreate}
+      options={options}
+      value={value}
+    />
+  )
+}
+
+ 
+
 const IngredientsTable = () => {
-  const [ingredients, setIngredients] = useState([
-    {
-      name: 'Wormwood',
-      description: 'Bittering agent',
-      amount: '1',
-      isDry: true,
-      id: '1',
-    },
-    {
-      name: 'Vodka',
-      description: 'solvent',
-      amount: '300',
-      isDry: false,
-      id: '2',
-    },
-    {
-      name: 'Orange peel',
-      description: 'citrus fruit',
-      amount: '6',
-      isDry: true,
-      id: '3',
-    },
-  ])
+  const [ingredients, setIngredients] = useState([])
+  const [showIngredientForm, setShowIngredientForm] = useState(false)
+
+  const changeAmount = (id, amount) => {
+    setIngredients(ingredients
+      .map(ingredient=>
+        id === ingredient.id
+          ? {...ingredient, amount: amount}
+          : ingredient
+      ))
+  }
 
   const handleDragEnd = (event) => {
     const {active, over} = event
@@ -86,7 +148,11 @@ const IngredientsTable = () => {
       })
     }
   }
-  
+
+  const handleRemoveIngredient = (id) => {
+    setIngredients(ingredients.filter(ingredient => ingredient.id !== id))
+  }
+
   return (
     <DndContext
       collisionDetection={closestCenter}
@@ -108,16 +174,22 @@ const IngredientsTable = () => {
             strategy={verticalListSortingStrategy}
           >
             {ingredients.map(ingredient => (
-              <DraggableRow key={ingredient.id} ingredient={ingredient}/>
+              <DraggableRow 
+                key={ingredient.id} 
+                ingredient={ingredient}
+                handleRemoveIngredient={handleRemoveIngredient}
+                changeAmount={changeAmount}
+              />
             ))}
 
           </SortableContext>
 
        
-
           <tr>
-            <td className='text-center' colSpan='100%'>
-            ADD NEW
+            <td colSpan='100%' className='text-center'>
+              {showIngredientForm 
+                ? <CreatableIngredient /> 
+                : <button onClick={() => setShowIngredientForm(true)}>Add an Ingredient</button> }
             </td>
           </tr>
 
