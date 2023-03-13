d52a7ddb08801cf63a3d6b5e191a98d6b4de9326
sasha v <volfym79@gmail.com>
2023-03-13 18:04:30 +0100

    init eslint to backend

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': 0,
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
